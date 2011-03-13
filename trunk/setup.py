902d08f072c0254982ebb9d8413dda9a5f37344e
evaduk@gmail.com <evaduk@gmail.com@15dbd72f-491f-e4d0-7100-b639e5b5bc5a>
2011-03-13 21:44:08 +0000

    Modified setup.py

--- trunk/setup.py
+++ trunk/setup.py
@@ -4,10 +4,14 @@ from distutils.core import setup
 
 setup(name='scs',
       version='1.0',
-      summary='Subversion Configuration System',
-      licence='GPL3',
+      license='GPL3',
       description='A unix system configuration tool based around subversion',
-      platform='Linux',
+      long_description='''SCS is a configuration management system for Unix 
+      systems such as servers and workstations. SCS is a lightweight system. 
+      It enforces strong use of version control (via Subversion) and, unlike 
+      systems like CFengine and Puppet, it does not force you to write code 
+      which is idempotent - instead SCS allows you to choose between 
+      "Transactional" mode and "Idempotent" mode.''',
       url='http://code.google.com/p/scs/',
       author='David Bell',
       author_email='dave@evad.info',
