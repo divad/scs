8571bc556e0a4980d69c039136835c905d520243
David Bell <divad@users.noreply.github.com>
2011-03-24 22:36:43 +0000

    Better scsinit, now supports user and pass

--- scslib.py
+++ scslib.py
@@ -30,7 +30,7 @@ import pysvn
 ################################################################################
 
 def version():
-	return 14
+	return 15
 	
 def versionStr():
 	return str(version())
