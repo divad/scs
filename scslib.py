d4bdb930d8c244af92f1065f8ca0ee4ce56a2c20
David Bell <divad@users.noreply.github.com>
2011-03-27 12:09:55 +0000

    Fixed more format bugs

--- scslib.py
+++ scslib.py
@@ -30,7 +30,7 @@ import pysvn
 ################################################################################
 
 def version():
-	return 27
+	return 28
 	
 def versionStr():
 	return str(version())
