3ebc8b667b38b4506d43fa2ee75d6b18897e5265
David Bell <divad@users.noreply.github.com>
2011-04-03 16:29:01 +0000

    scsp list is now sorted by package name

--- scslib.py
+++ scslib.py
@@ -30,7 +30,7 @@ import pysvn
 ################################################################################
 
 def version():
-	return 35
+	return 36
 	
 def versionStr():
 	return str(version())
