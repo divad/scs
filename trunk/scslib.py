224600379756907149921074a7abee93d1a2cdc9
evaduk@gmail.com <evaduk@gmail.com@15dbd72f-491f-e4d0-7100-b639e5b5bc5a>
2011-04-03 16:21:21 +0000

    Added scsp clone command

--- trunk/scslib.py
+++ trunk/scslib.py
@@ -30,7 +30,7 @@ import pysvn
 ################################################################################
 
 def version():
-	return 33
+	return 34
 	
 def versionStr():
 	return str(version())
