481e4be661fe7b8b1ea4c7d73c2e776719b1a57d
evaduk@gmail.com <evaduk@gmail.com@15dbd72f-491f-e4d0-7100-b639e5b5bc5a>
2011-04-03 16:23:40 +0000

    Changed output format of scsp list

--- trunk/scslib.py
+++ trunk/scslib.py
@@ -30,7 +30,7 @@ import pysvn
 ################################################################################
 
 def version():
-	return 34
+	return 35
 	
 def versionStr():
 	return str(version())
