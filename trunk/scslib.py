d4aac6d0d3616c9208b4d16d559454be0ca3bd7a
evaduk@gmail.com <evaduk@gmail.com@15dbd72f-491f-e4d0-7100-b639e5b5bc5a>
2011-06-05 12:15:52 +0000

    Bumped to version 43 of scs, added skeleton code for in-development SCS Package Editor gui

--- trunk/scslib.py
+++ trunk/scslib.py
@@ -30,7 +30,7 @@ import pysvn
 ################################################################################
 
 def version():
-	return 42
+	return 43
 	
 def versionStr():
 	return str(version())
