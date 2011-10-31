ddd771ebef199d902f9299fc6d9fcfe4891d35d5
David Bell <divad@users.noreply.github.com>
2011-10-31 10:34:59 +0000

    New release: 52, correction from earlier commit

--- scslib.py
+++ scslib.py
@@ -31,7 +31,7 @@ import stat
 ################################################################################
 
 def version():
-	return 51
+	return 52
 	
 def versionStr():
 	return str(version())
