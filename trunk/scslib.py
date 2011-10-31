b30e87a3da4bc1eb539eaceca2df32528a09f8be
evaduk@gmail.com <evaduk@gmail.com@15dbd72f-491f-e4d0-7100-b639e5b5bc5a>
2011-10-31 14:54:34 +0000

    New release: 53, bug fix in scsinit

--- trunk/scslib.py
+++ trunk/scslib.py
@@ -31,7 +31,7 @@ import stat
 ################################################################################
 
 def version():
-	return 52
+	return 53
 	
 def versionStr():
 	return str(version())
--- trunk/setup.py
+++ trunk/setup.py
@@ -3,7 +3,7 @@
 import distutils.core
 
 distutils.core.setup(name='scs',
-      version='52',
+      version='53',
       license='GPL3',
       description='A unix system configuration tool based around subversion',
       long_description='''SCS is a configuration management system for Unix 
